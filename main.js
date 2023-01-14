54a463c80be9553b749567754f7ed7ee3126d8df
wael35nl <waelrayya30nl@gmail.com>
2023-01-14 15:27:42 +0100

    functionality improved

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ const navList1 = document.getElementsByClassName('fa-list')[1];
 const navLinks1 = document.getElementsByClassName('nav-links')[1];
 
 navList.addEventListener('click', () => {
-    if (navLinks.style.display === 'none') {
-        navLinks.style.display = 'flex'
-    } else {
+    if (navLinks.style.display === 'flex') {
         navLinks.style.display = 'none'
+    } else {
+        navLinks.style.display = 'flex'
     }
 });
 
 navList1.addEventListener('click', () => {
-    if (navLinks1.style.display === 'none') {
-        navLinks1.style.display = 'flex'
-    } else {
+    if (navLinks1.style.display === 'flex') {
         navLinks1.style.display = 'none'
+    } else {
+        navLinks1.style.display = 'flex'
     }
 });
